e6e0c462e590a727432717683d9273a5280bbb40
Colin Ozanne <colin.ozanne@etudiant.univ-rennes.fr>
2025-09-19 12:34:51 +0200

    partie 6

--- model.js
+++ model.js
@@ -15,10 +15,42 @@ function Rectangle(colour, thickness, x, y, width, height) {
     this.topLeft = { x: x, y: y };
     this.width = width;
     this.height = height;
+
+    this.getInitX = function () {
+        return this.topLeft.x;
+    }.bind(this);
+
+    this.getInitY = function () {
+        return this.topLeft.y;
+    }.bind(this);
+
+    this.getFinalX = function () {
+        return this.topLeft.x + this.width;
+    }.bind(this);
+
+    this.getFinalY = function () {
+        return this.topLeft.y + this.height;
+    }.bind(this);
 }
 
 function Line(colour, thickness, x1, y1, x2, y2) {
     Shape.call(this, colour, thickness);
     this.start = { x: x1, y: y1 };
     this.end = { x: x2, y: y2 };
+
+    this.getInitX = function () {
+        return this.start.x;
+    }.bind(this);
+
+    this.getInitY = function () {
+        return this.start.y;
+    }.bind(this);
+
+    this.getFinalX = function () {
+        return this.end.x;
+    }.bind(this);
+
+    this.getFinalY = function () {
+        return this.end.y;
+    }.bind(this);
 }
--- view.js
+++ view.js
@@ -1,2 +1,37 @@
-
 // Implémenter ici les fonctions paint à ajouter dans chacune des classes du modèle.
+
+Shape.prototype.paint = function (ctx) {
+    ctx.strokeStyle = this.colour;
+    ctx.lineWidth = this.thickness;
+};
+
+Rectangle.prototype.paint = function (ctx) {
+    //TODO Manager color
+    ctx.beginPath();
+    ctx.rect(
+        this.getInitX(),
+        this.getInitY(),
+        this.getFinalX(),
+        this.getFinalY(),
+    );
+    ctx.stroke();
+};
+
+Line.prototype.paint = function (ctx) {
+    //TODO Manager color
+    ctx.beginPath();
+    ctx.moveTo(this.getInitX(), this.getInitY());
+    ctx.lineTo(this.getFinalX(), this.getFinalY());
+    ctx.stroke();
+};
+
+Drawing.prototype.paint = function (ctx) {
+    //console.log(this.getForms());
+    ctx.fillStyle = "#F0F0F0"; // set canvas' background color
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    for (const elem in this.getForms()) {
+        // now fill the canvas
+        elem.paint(ctx);
+    }
+};
