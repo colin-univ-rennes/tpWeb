4acff9d710249a78dba0c2575e3079b1667932e4
Colin Ozanne <colin.ozanne@etudiant.univ-rennes.fr>
2025-09-19 12:15:55 +0200

    partie 5

--- model.js
+++ model.js
@@ -1,3 +1,24 @@
-
 // Implémenter ici les 4 classes du modèle.
 // N'oubliez pas l'héritage !
+
+function Drawing() {
+    this.shapes = [];
+}
+
+function Shape(colour, thickness) {
+    this.colour = colour;
+    this.thickness = thickness; // in px
+}
+
+function Rectangle(colour, thickness, x, y, width, height) {
+    Shape.call(this, colour, thickness);
+    this.topLeft = { x: x, y: y };
+    this.width = width;
+    this.height = height;
+}
+
+function Line(colour, thickness, x1, y1, x2, y2) {
+    Shape.call(this, colour, thickness);
+    this.start = { x: x1, y: y1 };
+    this.end = { x: x2, y: y2 };
+}
