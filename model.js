8d372a465568db8862a049e10466d07ff03f7d0b
Colin Ozanne <colin.ozanne@etudiant.univ-rennes.fr>
2025-09-25 16:24:47 +0200

    partie 8

--- model.js
+++ model.js
@@ -6,10 +6,18 @@ function Drawing() {
 
     this.add = function (shape) {
         this.shapes.push(shape);
+        updateShapeList(this);
     }.bind(this);
 
     this.getShapes = function () {
         return this.shapes;
+    }.bind(this);
+
+    this.remove = function (index) {
+        if (index !== -1) {
+            this.shapes.splice(index, 1);
+            updateShapeList(this);
+        }
     }.bind(this);
 }
 
@@ -27,6 +35,28 @@ function Rectangle(colour, thickness, x, y) {
     this.update = function ({ x, y }) {
         this.width = x - this.topLeft.x;
         this.height = y - this.topLeft.y;
+    }.bind(this);
+
+    this.display = function () {
+        const title = document.createElement("span");
+        title.textContent = `Rectangle`;
+
+        const colour = document.createElement("span");
+        colour.textContent = this.colour;
+        colour.style.mixBlendMode = "difference";
+        colour.style.color = "white";
+
+        const container = document.createElement("span");
+        container.style.display = "inline-block";
+        container.style.width = "fit-content";
+        container.style.height = "fit-content";
+        container.style.backgroundColor = this.colour;
+        container.appendChild(colour);
+
+        const rest = document.createElement("span");
+        rest.textContent = `(${this.thickness}, ${this.topLeft.x}, ${this.topLeft.y}, ${this.width}, ${this.height})`;
+
+        return [title, container, rest];
     }.bind(this);
 }
 
@@ -54,5 +84,27 @@ function Line(colour, thickness, x, y) {
     this.update = function ({ x, y }) {
         this.end.x = x;
         this.end.y = y;
+    }.bind(this);
+
+    this.display = function () {
+        const title = document.createElement("span");
+        title.textContent = `Line`;
+
+        const colour = document.createElement("span");
+        colour.textContent = this.colour;
+        colour.style.mixBlendMode = "difference";
+        colour.style.color = "white";
+
+        const container = document.createElement("span");
+        container.style.display = "inline-block";
+        container.style.width = "fit-content";
+        container.style.height = "fit-content";
+        container.style.backgroundColor = this.colour;
+        container.appendChild(colour);
+
+        const rest = document.createElement("span");
+        rest.textContent = `(${this.thickness}, ${this.start.x}, ${this.start.y}, ${this.end.x}, ${this.end.y})`;
+
+        return [title, container, rest];
     }.bind(this);
 }
--- view.js
+++ view.js
@@ -31,3 +31,38 @@ Drawing.prototype.paint = function (ctx) {
         elem.paint(ctx);
     }
 };
+
+function updateShapeList(drawing) {
+    const shapeList = document.getElementById("shapeList");
+    shapeList.innerHTML = "";
+    const shapes = drawing.getShapes();
+    for (let i = 0; i < shapes.length; i++) {
+        const shape = shapes[i];
+
+        const deleteButtonContent = document.createElement("span");
+        deleteButtonContent.className = "glyphicon glyphicon-remove-sign";
+
+        const deleteButton = document.createElement("button");
+        deleteButton.type = "button";
+        deleteButton.className = "btn btn-default";
+        deleteButton.appendChild(deleteButtonContent);
+        deleteButton.addEventListener("click", () => {
+            shapeList.removeChild(li);
+            drawing.remove(i);
+            drawing.paint(ctx);
+        });
+
+        const li = document.createElement("li");
+        li.appendChild(deleteButton);
+        shape.display().forEach((element) => {
+            li.appendChild(element);
+        });
+        li.style.display = "flex";
+        li.style.flexDirection = "row";
+        li.style.alignItems = "center";
+        li.style.justifyContent = "start";
+        li.style.gap = "1rem";
+
+        shapeList.appendChild(li);
+    }
+}
